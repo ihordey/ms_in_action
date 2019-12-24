125420617488d8d76201c7ebc1813d66ee1422b5
ihordey <ihordey@gmail.com>
2019-12-24 22:04:30 +0200

    auth/oauth/token  response token

--- spmia-chapter7-master/authentication-service/src/main/java/com/thoughtmechanix/authentication/config/ServiceConfig.java
+++ spmia-chapter7-master/authentication-service/src/main/java/com/thoughtmechanix/authentication/config/ServiceConfig.java
@@ -1,12 +1,13 @@
 package com.thoughtmechanix.authentication.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 @Component
 @Configuration
 public class ServiceConfig {
-    //  @Value("${signing.key}")
+    @Value("${signing.key}")
     private String jwtSigningKey = "";
 
 
