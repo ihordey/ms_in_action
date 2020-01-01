6dbbee5c4c97c069c178aa9131019eed90a16806
ihordey <ihordey@gmail.com>
2020-01-01 21:28:38 +0200

    successful  get organization info use oauth server

--- spmia-chapter7-master/organization-service/src/main/java/com/thoughtmechanix/organization/security/ResourceServerConfiguration.java
+++ spmia-chapter7-master/organization-service/src/main/java/com/thoughtmechanix/organization/security/ResourceServerConfiguration.java
@@ -2,7 +2,6 @@ package com.thoughtmechanix.organization.security;
 
 
 import org.springframework.context.annotation.Configuration;
-
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
@@ -12,12 +11,12 @@ public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter
 
 
     @Override
-    public void configure(HttpSecurity http) throws Exception{
+    public void configure(HttpSecurity http) throws Exception {
         http
-        .authorizeRequests()
-          .antMatchers(HttpMethod.DELETE, "/v1/organizations/**")
-          .hasRole("ADMIN")
-          .anyRequest()
-          .authenticated();
+                .authorizeRequests()
+                .antMatchers(HttpMethod.DELETE, "/v1/organizations/**")
+                .hasRole("ADMIN")
+                .anyRequest()
+                .authenticated();
     }
 }
