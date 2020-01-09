f67693f3b7e2951930852c6d4d27b653676c8f3a
IHordey <P@ssw0rd!1981>
2020-01-09 08:33:22 +0200

    extract organizationId from JWT in auth-service

--- spmia-chapter7-master/authentication-service/src/main/java/com/thoughtmechanix/authentication/repository/OrgUserRepository.java
+++ spmia-chapter7-master/authentication-service/src/main/java/com/thoughtmechanix/authentication/repository/OrgUserRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrgUserRepository extends CrudRepository<UserOrganization,String>  {
-    public UserOrganization findByUserName(String userName);
+public interface OrgUserRepository extends CrudRepository<UserOrganization, String> {
+    UserOrganization findByUserName(String userName);
 }
--- spmia-chapter7-master/zuulsvr/src/main/java/com/thoughtmechanix/zuulsvr/filters/TrackingFilter.java
+++ spmia-chapter7-master/zuulsvr/src/main/java/com/thoughtmechanix/zuulsvr/filters/TrackingFilter.java
@@ -3,17 +3,17 @@ package com.thoughtmechanix.zuulsvr.filters;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.thoughtmechanix.zuulsvr.config.ServiceConfig;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import javax.xml.bind.DatatypeConverter;
-
 @Component
-public class TrackingFilter extends ZuulFilter{
-    private static final int      FILTER_ORDER =  1;
-    private static final boolean  SHOULD_FILTER=true;
+public class TrackingFilter extends ZuulFilter {
+    private static final int FILTER_ORDER = 1;
+    private static final boolean SHOULD_FILTER = true;
 
     private static final Logger logger = LoggerFactory.getLogger(TrackingFilter.class);
 
@@ -37,30 +37,46 @@ public class TrackingFilter extends ZuulFilter{
         return SHOULD_FILTER;
     }
 
-    private boolean isCorrelationIdPresent(){
-      if (filterUtils.getCorrelationId() !=null){
-          return true;
-      }
+    private boolean isCorrelationIdPresent() {
+        if (filterUtils.getCorrelationId() != null) {
+            return true;
+        }
 
-      return false;
+        return false;
     }
 
-    private String generateCorrelationId(){
+    private String generateCorrelationId() {
         return java.util.UUID.randomUUID().toString();
     }
 
+    private String getOrganizationId() {
+        if (filterUtils.getAuthToken() != null) {
+            String authToken = filterUtils.getAuthToken().replace("Bearer ", "");
+            try {
+                Claims claims = Jwts.parser()
+                        .setSigningKey(serviceConfig.getJwtSigningKey().getBytes("UTF-8"))
+                        .parseClaimsJws(authToken).getBody();
+                return (String) claims.get("organizationId");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return "";
+    }
+
     public Object run() {
-
         RequestContext ctx = RequestContext.getCurrentContext();
-
         if (isCorrelationIdPresent()) {
             logger.debug("tmx-correlation-id found in tracking filter: {}. ", filterUtils.getCorrelationId());
-        }
-        else{
+        } else {
             filterUtils.setCorrelationId(generateCorrelationId());
             logger.debug("tmx-correlation-id generated in tracking filter: {}.", filterUtils.getCorrelationId());
         }
 
+        String organizationId = getOrganizationId();
+        System.out.println("The organization id from the token is : " + organizationId);
+        filterUtils.setOrgId(organizationId);
+        logger.debug("Processing incoming request for {}.", ctx.getRequest().getRequestURI());
         return null;
     }
 }
